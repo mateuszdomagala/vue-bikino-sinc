f387fdb81fd1b44ad375727d9c08854f9cc3696b
unknown <kontakt@mateuszdomagala.pl>
2022-07-04 13:16:20 +0200

    feat: add BaseNumber component

---
+++ src/components/BaseNumber.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+type Numbers = "01" | "02" | "03" | "04" | "05" | "06" | "07" | "08" | "09";
+
+defineProps({
+  number: {
+    type: String as PropType<Numbers>,
+    default: "01",
+    validator: (value: string) =>
+      ["01", "02", "03", "04", "05", "06", "07", "08", "09"].includes(value),
+  },
+});
+</script>
+
+<template>
+  <div class="number">{{ number }}</div>
+</template>
+
+<style lang="scss" scoped>
+.number {
+  font-size: 4.7rem;
+  font-family: "Ubuntu", sans-serif;
+  font-weight: 700;
+  display: inline;
+  position: relative;
+  color: var(--color-primary);
+
+  @media (min-width: 992px) {
+    font-size: 2rem;
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 2.6rem;
+    width: 1.4rem;
+    background-color: var(--background-color-primary);
+
+    @media (min-width: 992px) {
+      height: 1.1rem;
+      width: 0.6rem;
+    }
+  }
+}
+</style>
--- src/components/__tests__/BaseButton.spec.ts
+++ src/components/__tests__/BaseButton.spec.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 import BaseButton from "../BaseButton.vue";
 
-describe("BaseButton", () => {
+describe("BaseButton.vue", () => {
   it("variant prop is required and must be a string", () => {
     expect(BaseButton.props).toMatchObject({
       variant: {
