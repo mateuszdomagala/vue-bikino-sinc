ba48d6f474aefbd1917c1d4797aec162cc909761
unknown <kontakt@mateuszdomagala.pl>
2022-07-24 14:54:07 +0200

    feat: add shop view

--- src/composables/useProducts.ts
+++ src/composables/useProducts.ts
@@ -7,24 +7,28 @@ export const useProducts = (params = {}) => {
   const products = ref<Product[] | null>([]);
   const metadata = ref<PaginationMeta | null>(null);
   const error = ref<string | null>(null);
+  const isLoading = ref<boolean>(false);
 
-  const fetchData = async () => {
+  const fetchData = async (params = {}) => {
     products.value = null;
     metadata.value = null;
     error.value = null;
+    isLoading.value = true;
 
     try {
       const { data, meta } = await commerce.products.list(params);
       products.value = data;
       metadata.value = meta;
+      isLoading.value = false;
     } catch (err) {
       error.value = `There is an error getting products, error: ${
         (err as Error).message
       }`;
+      isLoading.value = false;
     }
   };
 
-  fetchData();
+  fetchData(params);
 
-  return { products, metadata, error, fetchData };
+  return { products, metadata, error, isLoading, fetchData };
 };
