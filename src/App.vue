c1f01683fd23b40f57ee7908a05f039729f7600f
unknown <kontakt@mateuszdomagala.pl>
2022-08-16 14:38:15 +0200

    feat: add header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
+  <TheHeader :cart-total-items="5" />
   <RouterView />
 </template>
 
