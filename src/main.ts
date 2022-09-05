f98318a97e758076ac794f3a485d084e09b47731
unknown <kontakt@mateuszdomagala.pl>
2022-09-05 18:26:31 +0200

    feat: add google map with store locations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueGoogleMaps, { load: { key: import.meta.env.VITE_GOOGLE_MAPS_KEY } });
 
 app.mount("#app");
