eba970d6692884608e835a444f5470f52e1335db
Mateusz Domagała <kontakt@mateuszdomagala.pl>
2022-09-08 10:56:17 +0200

    feat: add 404/page not found (#26)

--- src/composables/useProduct.ts
+++ src/composables/useProduct.ts
@@ -1,10 +1,16 @@
 import { ref } from "vue";
 import { commerce } from "@/chec/commerce";
 import type { Product } from "@chec/commerce.js/types/product";
+import { useRouter } from "vue-router";
+
+interface ErrorExtended extends Error {
+  statusCode: number;
+}
 
 export const useProduct = (productId: string) => {
   const product = ref<Product | null>(null);
   const error = ref<string | null>(null);
+  const router = useRouter();
 
   const fetchData = async () => {
     product.value = null;
@@ -14,6 +20,9 @@ export const useProduct = (productId: string) => {
       const data = await commerce.products.retrieve(productId);
       product.value = data;
     } catch (err) {
+      if ((err as ErrorExtended).statusCode === 404) {
+        router.push({ name: "404" });
+      }
       error.value = `There is an error getting product, error: ${
         (err as Error).message
       }`;
